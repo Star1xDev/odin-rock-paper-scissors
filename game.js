b96fa27eb08cd582ea10f5108a398381966d4208
Star1xDev <abdastar2@gmail.com>
2022-08-08 14:35:55 +0100

    added function playRound
    function to play one round of Rock Paper Scissors

--- game.js
+++ game.js
@@ -5,3 +5,65 @@ function getComputerChoice(){
   let randomIndex = Math.floor(Math.random() * 3 );
   return choices[randomIndex];
 }
+
+
+
+// function to play one round of Rock,Paper,Scissors
+
+function playRound(playerSelection, computerSelection){
+
+
+  // to uppercase to make choice case-insensitive
+  computerSelection = computerSelection.toUpperCase();
+  playerSelection = playerSelection.toUpperCase();
+
+  console.log(`player choice is : ${playerSelection}`);
+  console.log(`computer choice is : ${computerSelection}`);
+
+
+  // if player inputs invalid choice
+  if(playerSelection !== "ROCK" && playerSelection !== "PAPER" && playerSelection !== "SCISSORS"){
+    return "Invalid Choice!!!";
+  }
+
+  // if player and computer pick same choice
+  if(playerSelection === computerSelection){
+    return "Tie!";
+  }
+
+  // if player picks rock
+  if(playerSelection === "rock".toUpperCase()){
+    if (computerSelection === "scissors".toUpperCase()) {
+      return "You Win! Rock beats Scissors";
+    }
+    if (computerSelection === "paper".toUpperCase()) {
+      return "You Lose! Paper beats Rock";
+    }
+  }
+
+  // if player picks paper
+  if (playerSelection === "paper".toUpperCase()) {
+    if (computerSelection === "rock".toUpperCase()) {
+      return "You Win! paper beats rock";
+    }
+    if (computerSelection === "scissors".toUpperCase()) {
+      return "You Lose! Scissors beats Paper";
+    }
+  }
+
+  // if player picks scissors
+  if(playerSelection === "scissors".toUpperCase()){
+    if (computerSelection === "paper".toUpperCase()) {
+      return "You Win! Scissors beats Paper";
+    }
+    if (computerSelection === "rock".toUpperCase()) {
+      return "You Lose! Rock beats Scissors";
+    }
+  }
+}
+
+
+let playerChoice = prompt("Enter Your Choice : ");
+let computerChoice = getComputerChoice();
+
+console.log(playRound(playerChoice,computerChoice));
